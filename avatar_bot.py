35d216c522ad3194f76ecb4b24b523f74cedde09
Grig10 <chujkin@inbox.ru>
2021-09-13 20:52:23 +0300

    Add files via upload
    added telegram bot

--- avatar_bot.py
+++ avatar_bot.py
@@ -1,49 +1,76 @@
-import requests
+import re
 import json
-import os
+import subprocess
+import time
+import webbrowser
+import requests
+import telebot
 from requests.structures import CaseInsensitiveDict
 
-def extract_id_from_data(data):
-    dictData = json.loads(json.dumps(data))
-    return dictData["id"]
+import config
 
-def request_to_translate_text(id, text):
-    url = "http://84.201.181.102:8090/api/v1/avatar/" + id + "/translate/text/"
+bot = telebot.TeleBot("1985018161:AAHzdQisPA_B0Tx9i48OegX139QbWGvpH4o")
+
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    if message.text == "/start":
+        bot.send_message(message.chat.id, "Добро пожаловать,  {0.first_name}!\nЯ - {1.first_name}, переводчик русского "
+                                          "жестового языка. "
+                                          "Я буду переводить любую вашу фразу или слово в жесты. Напишите здесь.".format(
+            message.from_user, bot.get_me()))
+    elif message.text == "/help":
+        bot.send_message(message.chat.id, "Напишите любую фразу или слово")
+
+
+@bot.message_handler(content_types=['text'])
+def translate_text(message):
+    if re.search(r'[^а-я А-Я ?!."]', message.text):
+        bot.send_message(message.chat.id, "Напишите только русскими буквами, пожалуйста")
+    else:
+        bot.send_message(message.chat.id, "Запрос отправлен. Ожидайте ответа, пожалуйста.")
+        dict_avatar = json.loads(json.dumps(take_avatar()))
+        id_streaming = dict_avatar["id"]
+        url_streaming = dict_avatar["streaming_url"]
+        if request_to_translate_text(id_streaming, url_streaming,
+                                     message.text.strip()):
+            video = open('video.mp4', 'rb')
+            bot.send_video(message.chat.id, video, timeout=50)
+        else:
+            bot.send_message(message.chat.id, "Произошла ошибка с запросом. Повторите, пожалуйста.")
+
+
+def request_to_translate_text(_id, _streaming, text):
+    url_avatar = "http://84.201.181.102:8090/api/v1/avatar/" + _id + "/translate/text/"
+    header = CaseInsensitiveDict()
+    header["accept"] = "application/json"
+    header["Content-Type"] = "application/json"
+    resp_data = {"text": text, "speed": 1}
+    response = requests.post(url_avatar, headers=header, json=resp_data)
+    if response.status_code == 200:
+        webbrowser.get('firefox').open(_streaming)
+        proc = subprocess.Popen(
+            [
+                """ffmpeg -f avfoundation -i "1:0" -vf "crop=2500:1400:500:500" -pix_fmt yuv420p -y -r 10 video.mp4"""],
+            shell=True)
+        time.sleep(10)
+        proc.terminate()
+        time.sleep(5)
+        return True
+    else:
+        return False
+
+
+def take_avatar():
+    url = "http://84.201.181.102:8090/api/v1/avatar/take/"
     headers = CaseInsensitiveDict()
     headers["accept"] = "application/json"
-    headers["Content-Type"] = "application/json"
-    data = {"text": text, "speed": 1}
-    resp = requests.post(url, headers=headers, json=data)  
-    result = resp.json()
-    resp.close()
-    return result
-    
-
-url = "http://84.201.181.102:8090/api/v1/avatar/take/"
-
-headers = CaseInsensitiveDict()
-
-headers["accept"] = "application/json"
-
-resp = requests.get(url, headers=headers)
- 
-if resp.status_code == 200:
-   id = extract_id_from_data(resp.json())
-   resp.close()
-else:
-    print(resp.raise_for_status())
-
-current_dir = os.getcwd() + "/Desktop"
-path_translate_text = os.path.join(current_dir, 'translate_text.txt')
-path_result_text = os.path.join(current_dir, 'result.txt')
-with open(path_translate_text, 'r', encoding='cp1251', errors='ignore') as f:
-    data = f.read()
-
-result = request_to_translate_text(id, data.strip())
-
-with open(path_result_text, 'w', encoding='cp1251', errors='ignore') as f:
-    json.dump(result, f, ensure_ascii=False)
+    resp = requests.get(url, headers=headers)
+    if resp.status_code == 200:
+        return resp.json()
+    else:
+        print("Received unexpected status code {}".format(resp.status_code))
 
 
-
-
+if __name__ == '__main__':
+        bot.polling(none_stop=True)
